8220956b0b0ae81b0685eaf4637e7cc5e6dbf689
Kalle <qalle85@gmail.com>
2019-01-20 05:44:54 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,9 @@
 @echo off
 cls
 python dircomp.py test1 test2
+echo.
+echo === test.bat: these should cause errors ===
+python dircomp.py
+python dircomp.py x y
+python dircomp.py .
+python dircomp.py . .
