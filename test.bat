dd136220ae5ff16c636fde3ae0d725d48d811869
Kalle <qalle85@gmail.com>
2020-03-20 22:25:58 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -6,30 +6,23 @@ python dircomp.py test1 test2
 echo.
 
 echo === test.bat: compare file contents ===
-python dircomp.py -c test1 test2
+python dircomp.py --compare-contents test1 test2
 echo.
 
-echo === test.bat: these should all cause syntax errors ===
-python dircomp.py
-python dircomp.py a b c
-python dircomp.py --nonexistent-arg a b
-echo.
-
-echo === test.bat: these should all cause "path not found" errors ===
-python dircomp.py nonexistent1
+echo === test.bat: these cause "path not found" errors ===
 python dircomp.py nonexistent1 nonexistent2
 python dircomp.py test1 nonexistent2
 echo.
 
-echo === test.bat: these should all cause "paths are the same" errors ===
-python dircomp.py .
-python dircomp.py test1/..
+echo === test.bat: these should all cause "one path is under another" errors ===
 python dircomp.py . .
+python dircomp.py test1/.. .
 python dircomp.py . test1/..
 python dircomp.py test1 ./test1
+python dircomp.py test1 .
+python dircomp.py ./test1 .
+python dircomp.py test1 test1/..
 echo.
 
-echo === test.bat: these should all cause "one path is under another" errors ===
-python dircomp.py test1
-python dircomp.py ./test1
-python dircomp.py test1 test1/..
+echo === test.bat: help text ===
+python dircomp.py --help
